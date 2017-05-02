3b8053c1b20aecbc4cab0028489c42e330603e4a
ddo6 <ddo6@njit.edu>
2017-05-02 19:25:12 -0400

    Update header.php

--- view/header.php
+++ view/header.php
@@ -1,30 +1,21 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
-    "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
-    <!-- the head section -->
-    <head>
-        <title>SportsPro Technical Support</title>
-        
-        <link rel="stylesheet" type="text/css"
-              href="/~ddo6/is218finalproject/css/main.css" />
-      
-        
-        <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
-        
-        <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,700" rel="stylesheet">
-        <link href="//fonts.googleapis.com/css?family=Raleway:400,300,600" rel="stylesheet" type="text/css">
-            
-        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
-        <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
-        <script src="<?php echo $app_path ?>/js/scripts.js"></script>
-    </head>
+<!DOCTYPE html>
+<html>
 
-    <!-- the body section -->
-    <body>
-    <div id="page" class="container">
-        <div id="header" class="twelve columns">
-            <h1>SportsPro Technical Support</h1>
-            <p>Sports management software for the sports enthusiast</p>
-            <ul class="nav"><li><a href="<?php echo $app_path ?>">Home</a></li></ul>
-        </div>
-       
+<!-- the head section -->
+<head>
+    <title>SportsPro Technical Support</title>
+    <link rel="stylesheet" type="text/css"
+          href="/~ddo6/218finalproject/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header>
+    <h1>SportsPro Technical Support</h1>
+    <p>Sports management software for the sports enthusiast</p>
+    <nav>
+        <ul>
+            <li><a href="/~ddo6/218finalproject/index.php">Home</a></li>
+        </ul>
+    </nav>
+</header>
