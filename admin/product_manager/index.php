601b0fa1fafdc2fe8d5fe1915eef72b328661afa
ddo6 <ddo6@njit.edu>
2017-05-10 16:51:14 -0400

    Update index.php

--- admin/product_manager/index.php
+++ admin/product_manager/index.php
@@ -1,58 +1,36 @@
 <?php
-require_once('../../util/main.php');
-require_once('../../util/secure_conn.php');
-require('../../model/database.php');
-require('../../model/product_db.php');
-
-$action = filter_input(INPUT_POST, 'action');
-if ($action == NULL) {
-    $action = filter_input(INPUT_GET, 'action');
-    if ($action == NULL) {
-        $action = 'list_products';
-    }
+require('../model/database.php');
+require('../model/product_db.php');
+if (isset($_POST['action'])) {
+    $action = $_POST['action'];
+} else if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+} else {
+    $action = 'list_products';
 }
-
-// products
-switch($action) {
-    case 'list_products':
-        // list all products
-        $products = get_all_products();
-        include('product_list.php');
-        break;
-    case $action == 'delete_product':
-        // delete a product
-        $code = filter_input(INPUT_POST, 'code');
-        if ($code == NULL || $code == FALSE) {
-            $error = "Missing or incorrect product code.";
-            include('../../errors/error.php');
-        } else { 
-            delete_product($code);
-            header("Location: .");
-        }
-        break;
-    case 'show_add_form':
-        // call page to add a product
-        include('product_add.php'); 
-        break;
-    case 'add_product':
-        // add a product
-        $code = filter_input(INPUT_POST, 'code');
-        $name = filter_input(INPUT_POST, 'name');
-        $version = filter_input(INPUT_POST, 'version');
-        $date = filter_input(INPUT_POST, 'date');
-        if ($code == NULL || $code == FALSE || 
-            $name == NULL || $name == FALSE || 
-            $version == NULL || $version == FALSE ||
-            $date == NULL || $date == FALSE) {
-            $error = "Invalid product data. Check all fields and try again.";
-            include('../../errors/error.php');
-        } else { 
-            add_product($code, $name, $version, $date);
-            header("Location: .");
-        }
-        break;
-    default:
-        display_error("Unknown action: " . $action);
-        break;
+if ($action == 'list_products') {
+    // Get product data
+    $products = get_products();
+    // Display the product list
+    include('product_list.php');
+} else if ($action == 'delete_product') {
+    $product_code = $_POST['product_code'];
+    delete_product($product_code);
+    header("Location: .");
+} else if ($action == 'show_add_form') {
+    include('product_add.php');
+} else if ($action == 'add_product') {
+    $code = $_POST['code'];
+    $name = $_POST['name'];
+    $version = $_POST['version'];
+    $release_date = $_POST['release_date'];
+    // Validate the inputs
+    if (empty($code) || empty($name) || empty($version) || empty($release_date)) {
+        $error = "Invalid product data. Check all fields and try again.";
+        include('../errors/error.php');
+    } else {
+        add_product($code, $name, $version, $release_date);
+        header("Location: .");
+    }
 }
 ?>
