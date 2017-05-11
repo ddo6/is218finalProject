6ee73418b0f2eabf9b6b32fc70ab77dc0de2945a
ddo6 <ddo6@njit.edu>
2017-05-11 18:19:06 -0400

    Update unassigned_incidents.php

--- admin/incident_display/unassigned_incidents.php
+++ admin/incident_display/unassigned_incidents.php
@@ -4,40 +4,33 @@
 
     <div id="main">
         <!-- display a table of incidents -->
-        <table class="outer_table">
+        <table>
             <tr>
                 <th>Customer</th>
                 <th>Product</th>
-                <th>Incident</th>
+                <th>Date Opened</th>
+                <th>Title</th>
+                <th>Description</th>
+                <th>&nbsp;</th>
             </tr>
             <?php foreach ($incidents as $incident) : ?>
             <tr>
                 <td><?php echo $incident['firstName'] . " ". $incident['lastName']; ?></td>
-                <td><?php echo $incident['name']; ?></td>
-                <td>
-                    <table class="inner_table">
-                        <tr>
-                            <td>ID:</td>
-                            <td><?php echo $incident['incidentID']; ?></td>
-                        </tr>
-                        <tr>
-                            <td>Opened:</td>
-                            <td><?php $dateOpened = new DateTime($incident['dateOpened']); 
-                                      echo $dateOpened->format('m/d/Y'); ?></td>
-                        </tr>
-                        <tr>
-                            <td>Title:</td>
-                            <td><?php echo $incident['title']; ?></td>
-                        </tr>
-                        <tr>
-                            <td>Description:</td>
-                            <td><?php echo $incident['description']; ?></td>
-                        </tr>
-                    </table>
-                </td>
+                <td><?php echo $incident['productCode']; ?></td>
+                <td><?php echo $incident['dateOpened']; ?></td>
+                <td><?php echo $incident['title']; ?></td>
+                <td><?php echo $incident['description']; ?></td>
+                <!-- select the incident -->
+                <td><form action="" method="post">
+                    <input type="hidden" name="action"
+                           value="show_select_technician">
+                    <input type="hidden" name="id"
+                           value="<?php echo $incident['incidentID']; ?>">
+                    <input type="submit" value="Select">
+                </form></td>
             </tr>
             <?php endforeach; ?>
-        </table>      
+        </table>            
     </div>
 </main>
 <?php include '../../view/footer.php'; ?>
