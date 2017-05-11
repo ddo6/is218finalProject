74f4903d236ed829f8e045d9d04f2d2ec8487d5d
ddo6 <ddo6@njit.edu>
2017-05-11 18:21:40 -0400

    Create admin_header.php

---
+++ view/admin_header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+
+<!-- the head section -->
+<head>
+    <title>SportsPro Technical Support</title>
+    <link rel="stylesheet" type="text/css"
+          href="/~ddo6/is218final/css/main.css">
+</head>
+
+<!-- the body section -->
+<body>
+<header>
+    <h1>SportsPro Technical Support</h1>
+    <p>Sports management software for the sports enthusiast</p>
+    <nav>
+        <ul>
+            <li><a href="/~ddo6/is218final/admin/index.php">Home</a></li>
+        </ul>
+    </nav>
+</header>
