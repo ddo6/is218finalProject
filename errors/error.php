0de109e3a30c456c6f6f4564dd399c833127425b
ddo6 <ddo6@njit.edu>
2017-05-12 16:26:32 -0400

    Update error.php

--- errors/error.php
+++ errors/error.php
@@ -1,4 +1,5 @@
 <?php include '../view/header.php'; ?>
+<!-- Error file to throw if generic error occurs -->
 <div id="main">
     <h1 class="top">Error</h1>
     <p><?php echo $error_message; ?></p>
